40492bae31f90c898c62cf49a4f3db6ac3d33de3
GraceYourCode <freshmannskuls@gmail.com>
2024-12-07 16:38:50 +0100

    Served the routes to vercel build folder

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import authRoutes from '../src/routes/authRoutes'
-import verificationRoutes from '../src/routes/verificationRoutes'
+import authRoutes from './routes/authRoutes'
+import verificationRoutes from './routes/verificationRoutes'
 
 const app = express();
 app.use(express.json());
